9f94fd1bfee748876c1cfcbdef591b97371eb13a
barrera19 <barreraman19@gmail.com>
2023-02-02 23:41:31 -0500

    Preguntas correctas
    Parte 3 Preguntas Correctas + Incorrectas

--- Assets/Scripts/RandomSelect.cs
+++ Assets/Scripts/RandomSelect.cs
@@ -6,9 +6,11 @@ public class RandomSelect : MonoBehaviour
 {
     [SerializeField] int numQuestions;
     public int count = 0;
+    public int countQuestions = 0;
     public List<int> numRandom;
     public bool stop = false;
     public GameObject[] questions;
+    public int points = 0;
 
 
     void Start()
@@ -16,28 +18,54 @@ public class RandomSelect : MonoBehaviour
          while (!stop)
          {  
             int temp = Random.Range(0, numQuestions - 1);
+
              if (!numRandom.Contains(temp))
              {
                  numRandom.Add(temp);
-                 count++;      
-             }
-              if (count  == numQuestions - 1)
-            {
-                stop = true;
-                //break;
-            }  
-                         
+                 count++;
+                 print("Count: " + count);    
+                 print("Numero Preguntas: " + numQuestions);
+
+             }  
+                  
+                if (count  == numQuestions -1)
+                {
+                    stop = true;
+                } 
+                        
          }
 
         ActiveQuestion();
 
         }
-
-        
-
     public void ActiveQuestion()
     {
-        questions[numRandom[0]].SetActive(true);
+        if(countQuestions != 0)
+        {
+          questions[numRandom[countQuestions]].SetActive(false);
+          countQuestions++;  
+          questions[numRandom[countQuestions]].SetActive(true);
+    
+        }
+        else
+        {
+            questions[numRandom[0]].SetActive(false);
+            questions[numRandom[countQuestions]].SetActive(true);
+            countQuestions++;
+        }
+    
     }
 
+    public void CorrectAnswer()
+    {
+        points = points + 100;
+        ActiveQuestion();
+
+    }
+
+
+    public void IncorrectAnwser()
+    {
+        ActiveQuestion();
+    }
 }
